47093340a35918ca68464ad8b55d290a3260f0cc
huhn511 <sehe89@gmail.com>
2020-05-06 03:34:01 +0200

    add start_a_new_channel

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,10 +1,14 @@
 use neon::prelude::*;
-use iota::Client;
+use iota_lib_rs::prelude::iota_client;
+
 use iota_streams::{
-    app_channels::api::tangle::{Address, Author, DefaultTW, Message},
-    core::tbits::Tbits,
-    protobuf3::types::Trytes,
+    app_channels::api::tangle::{Author},
 };
+use iota_streams::app::transport::tangle::client::SendTrytesOptions;
+
+use crate::api_author::announce::start_a_new_channel;
+mod api_author;
+
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello Node.js from Rust."))
@@ -24,11 +28,24 @@ impl Task for BackgroundTask {
     // thread
     fn perform(&self) -> Result<String, String> {
 
-        let seed = "YOURSEED99999999999999999999999999999999999999999999999999999999";
-        // tangle client
-        let mut api = Client::new("https://nodes.comnet.thetangle.org:443");
-        // Create the author
-        let mut author = Author::new(seed, 3, true);
+
+        // Create a new channel
+        let mut author = Author::new("AUTHORSECRET999", 3, true);
+
+        // Connect to a node and pass this object to the function
+        let mut client = iota_client::Client::new("https://nodes.devnet.iota.org:443");
+
+        // Change the default settings to use a lower minimum weight magnitude for the Devnet
+        let mut send_opt = SendTrytesOptions::default();
+
+        // Default MWM is 14
+        send_opt.min_weight_magnitude = 9; 
+
+        match start_a_new_channel(&mut author, &mut client, send_opt) {
+            Ok(()) => (),
+            Err(error) => println!("Failed with error {}", error),
+        }
+    
 
         println!(
             "Channel Adress = {}",
@@ -53,7 +70,8 @@ pub fn publish(mut cx: FunctionContext) -> JsResult<JsUndefined> {
 
     let f = cx.argument::<JsFunction>(0)?;
     BackgroundTask.schedule(f);
-   
+
+
     Ok(cx.undefined())
 }
 

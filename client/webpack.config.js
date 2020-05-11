74f2cf4aad3ef35675bc84ed23036b9fe63d0b6f
huhn511 <sehe89@gmail.com>
2020-05-11 15:13:51 +0200

    fix webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
+        library: 'wasm',
+        libraryExport: 'default'
     },
     plugins: [
         new HtmlWebpackPlugin({

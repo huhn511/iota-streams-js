0023eedefcede8f58ce475f8dca34b707f1b1af2
huhn511 <sehe89@gmail.com>
2020-05-11 13:46:24 +0200

    add wasm address generation by @Thoralf-M

--- client/index.js
+++ client/index.js
@@ -1,8 +1,26 @@
 // Note that a dynamic `import` statement here is required due to
 // webpack/webpack#6615, but in theory `import { greet } from './pkg';`
 // will work here one day as well!
-const rust = import('./pkg');
+const wasmmodule = import('./pkg');
+async function generateAddress(seed, index, security, checksum) {
+  try {
+    let wasm = await import('./pkg')
+    let address = wasm.new_address(seed, index, security, checksum)
+    console.log(address);
+  } catch (err) {
+    console.error(err)
+  }
+}
 
-rust
-  .then(m => m.greet('World!'))
-  .catch(console.error);
+async function showAlert() {
+  try {
+    wasmmodule
+      .then(m => m.greet('World!'))
+      .catch(console.error);
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+export default { showAlert, generateAddress, wasmmodule }
